e01f30232de0175c6bf5745f7613eac6d775a063
Solmaz <smaabi@student.unimelb.edu.au>
2025-11-15 11:00:36 +1100

    Update Chart.js

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,13 +1,39 @@
-import React from react;
-import { Line } from react-chartjs-2;
-import { Chart as ChartJS, CategoryScale, LinearScale, LineElement, PointElement } from chart.js;
+import React from "react";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+} from "chart.js";
 
-ChartJS.register(CategoryScale, LinearScale, LineElement, PointElement);
+// Register the required components
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 export default function Chart({ chartData }) {
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: { position: "top" },
+      title: { display: true, text: "Weekly Traffic" }
+    }
+  };
+
   return (
-    div className=chart-container
-      Line data={chartData} 
-    div
+    <div className="chart-container">
+      <Line data={chartData} options={options} />
+    </div>
   );
 }
